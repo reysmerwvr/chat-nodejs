3d28343ef4e31be4457e1ccfda5f9db1ee413f43
Reysmer Valle <reysmerwvr@gmail.com>
2016-02-23 16:57:31 +0000

    Users Chat

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express"),
     app = express(),
     server = require("http").createServer(app),
-    io = require("socket.io").listen(server);
+    io = require("socket.io").listen(server),
+    nicknames = {};
+    
     
     server.listen(process.env.PORT, process.env.IP);
     
@@ -10,9 +12,31 @@ var express = require("express"),
     app.get("/", function(req, res){
         res.sendfile(__dirname + "/index.html");    
     });
-    
+    // Sockets
     io.sockets.on('connection', function(socket){
         socket.on('sendMessage', function(data){
-            io.sockets.emit('newMessage', {msg : data})
+            io.sockets.emit('newMessage', {msg : data, nick : socket.nickname})
         });
+        
+        socket.on('newUser', function(data, callback) {
+            if(data in nicknames){
+                callback(false);
+            }else{
+                callback(true);
+                socket.nickname = data;
+                nicknames[socket.nickname] = 1;
+                updateNickNames();
+            }
+        });
+        
+        socket.on('disconnect', function(data) {
+            if(!socket.nickname) 
+                return;
+            delete nicknames[socket.nickname];
+            updateNickNames();
+        });
+        
+        function updateNickNames(){
+            io.sockets.emit('usernames', nicknames);
+        }
     });
